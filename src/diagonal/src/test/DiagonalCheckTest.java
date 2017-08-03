eb35e8c3cffa3ce2d4ade7f25daf4ebd8bd63948
Ricardocrespo <ricardocrespo@gmail.com>
2017-08-02 20:41:38 -0600

    Created test package

---
+++ src/diagonal/src/test/DiagonalCheckTest.java
@@ -0,0 +1,14 @@
+package diagonal.src.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DiagonalCheckTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
